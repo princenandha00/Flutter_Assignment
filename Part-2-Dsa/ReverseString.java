61695a08727938199529229d3619ec3ff87721a2
princenandha00 <princenandha00@gmail.com>
2026-06-11 21:50:56 +0530

    Update ReverseString.java

--- Part-2-Dsa/ReverseString.java
+++ Part-2-Dsa/ReverseString.java
@@ -1,1 +1,31 @@
+public class ReverseString {
 
+    public static String reverse(String str) {
+
+        char[] arr = str.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+
+        String str = "Flutter";
+
+        System.out.println(
+            reverse(str)
+        );
+    }
+}
