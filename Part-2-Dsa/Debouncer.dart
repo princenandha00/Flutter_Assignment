647f5494adb51c5b7a697bbdee87f19b785c7708
princenandha00 <princenandha00@gmail.com>
2026-06-11 21:50:24 +0530

    Update Debouncer.dart

--- Part-2-Dsa/Debouncer.dart
+++ Part-2-Dsa/Debouncer.dart
@@ -1,1 +1,31 @@
+import 'dart:async';
 
+class Debouncer {
+  final int milliseconds;
+  Timer? _timer;
+
+  Debouncer({
+    required this.milliseconds,
+  });
+
+  void run(VoidCallback action) {
+    _timer?.cancel();
+
+    _timer = Timer(
+      Duration(milliseconds: milliseconds),
+      action,
+    );
+  }
+}
+
+
+// Usage
+
+final debouncer =
+    Debouncer(milliseconds: 500);
+
+onChanged: (value) {
+  debouncer.run(() {
+    searchProduct(value);
+  });
+}
