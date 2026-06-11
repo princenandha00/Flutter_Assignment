2b77208c84cb09bfc7523722561d723863d7ad62
princenandha00 <princenandha00@gmail.com>
2026-06-11 21:51:27 +0530

    Update DuplicateElements.java

--- Part-2-Dsa/DuplicateElements.java
+++ Part-2-Dsa/DuplicateElements.java
@@ -1,1 +1,28 @@
+import java.util.HashSet;
 
+public class DuplicateElements {
+
+    public static void main(String[] args) {
+
+        int[] arr = {
+            1, 2, 3, 4, 2, 5, 3
+        };
+
+        HashSet<Integer> seen =
+            new HashSet<>();
+
+        HashSet<Integer> duplicates =
+            new HashSet<>();
+
+        for (int num : arr) {
+
+            if (!seen.add(num)) {
+                duplicates.add(num);
+            }
+        }
+
+        System.out.println(
+            "Duplicates: " + duplicates
+        );
+    }
+}
